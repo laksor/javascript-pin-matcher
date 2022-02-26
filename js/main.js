35e70d03681ca42e3a71cf711f43384f55932ebd
laskor <al.moksud@gmail.com>
2022-02-25 23:40:13 -0500

    pin verify function & alert added

--- js/main.js
+++ js/main.js
@@ -32,12 +32,12 @@ document.getElementById('key').addEventListener('click', function (event){
     }
     
 })
-
+// submit button pin verify and show alert 
 function verifyPin(){
     const pin = document.getElementById('display-pin').value;
     const typedNumber = document.getElementById('number').value;
     const success = document.getElementById('success');
-    const fail = document.getElementById('f');
+    const fail = document.getElementById('fail');
     if(pin == typedNumber){
         
         success.style.display = 'block';
